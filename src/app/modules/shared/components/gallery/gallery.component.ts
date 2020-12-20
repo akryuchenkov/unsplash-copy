19a04019744012739a5c9a78867f8eb5a899d007
a.kryuchenkov <a.kryuchenkov2@gmail.com>
2020-12-20 14:14:04 +0300

    gallery

--- src/app/modules/shared/components/gallery/gallery.component.ts
+++ src/app/modules/shared/components/gallery/gallery.component.ts
@@ -1,20 +1,59 @@
-import { Component, OnInit } from '@angular/core';
-import {UnsplashService} from '../../unsplash.service';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
+import { UnsplashService } from '../../unsplash.service';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.css']
+  styleUrls: ['./gallery.component.css'],
 })
 export class GalleryComponent implements OnInit {
+  @Input()
+  topic = '';
+  innerWidth = 1920;
+
   pictures = [];
   constructor(private unsplashService: UnsplashService) {
-      this.unsplashService.getListPhotos().subscribe(pictures => {
+    if (!this.topic) {
+      this.unsplashService.getListPhotos().subscribe((pictures) => {
         this.pictures = pictures;
       });
+    } else {
+      this.unsplashService
+        .getPhotosByTopic(this.topic)
+        .subscribe((pictures) => {
+          this.pictures = pictures;
+        });
+    }
+  }
+
+  get picturesInner() {
+    return chunkArray(this.pictures, this.pictures.length / this.columns);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerWidth = window.innerWidth;
+  }
+
+  get columns() {
+    if (this.innerWidth < 800) {
+      return 1;
+    }
+    return 3;
   }
 
   ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
+  }
+}
+
+function chunkArray(arr, chunkSize) {
+  let chunks = [];
+
+  for (let index = 0; index < arr.length; index += chunkSize) {
+    const chunk = arr.slice(index, index + chunkSize);
+    chunks.push(chunk);
   }
 
+  return chunks;
 }
--- src/app/modules/system/pages/page/page.component.ts
+++ src/app/modules/system/pages/page/page.component.ts
@@ -1,36 +1,17 @@
-import {Component, OnInit} from '@angular/core';
-import {UnsplashService} from '../../../shared/unsplash.service';
-import {Routes} from "@angular/router";
-import {AuthComponent} from '../../../auth/components/auth/auth.component';
-import {LoginComponent} from '../../../auth/components/login/login.component';
-import {RegisterComponent} from '../../../auth/components/register/register.component';
+import { Component, OnInit } from '@angular/core';
+import { UnsplashService } from '../../../shared/unsplash.service';
+import { Routes } from '@angular/router';
+import { AuthComponent } from '../../../auth/components/auth/auth.component';
+import { LoginComponent } from '../../../auth/components/login/login.component';
+import { RegisterComponent } from '../../../auth/components/register/register.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: AuthComponent,
-    children: [
-      {path: '', component: LoginComponent},
-      {path: 'join', component: RegisterComponent},
-      {path: '**', redirectTo: ''}
-    ],
-  },
-];
 @Component({
   selector: 'app-page',
   templateUrl: './page.component.html',
   styleUrls: ['./page.component.css'],
-
 })
 export class PageComponent implements OnInit {
+  constructor(private unsplashService: UnsplashService) {}
 
-
-  constructor(private unsplashService: UnsplashService) {
-
-  }
-
-  ngOnInit(): void {
-
-  }
-
+  ngOnInit(): void {}
 }
